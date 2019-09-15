770167a605d0a49310f70226a3ef23e4c3c2664d
Ben <senorgeorgie@yahoo.com>
2019-09-15 20:15:10 +0700

    Contoso University Tutorial
    This was to help familiarize me with ASP.NET MVC prior to doing the Live Project Course with the Tech Academy

---
+++ ContosoUniversity/DAL/SchoolContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ContosoUniversity.Models;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace ContosoUniversity.DAL
+{
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext() : base("SchoolContext")
+        {
+        }
+        
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Course> Courses { get; set; }
+
+        //protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        //{
+        //    modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        //    //This gets rid of plural names of tables...I don't think I want that
+        //}
+    }
+}
